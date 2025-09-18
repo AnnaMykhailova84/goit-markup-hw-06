7872ee69f6e454d52207d8d4f1b3a259cc23b1b7
AnnaM <amykhailova84@gmail.com>
2025-09-18 11:42:23 -0500

    add button to Mobile-Menu

--- js/modal.js
+++ js/modal.js
@@ -6,13 +6,24 @@
     closeModalBtn: document.querySelector('[data-modal-close]'),
     // Додати атрибут data-modal на бекдроп модалки
     modal: document.querySelector('[data-modal]'),
+    openMobileBtn: document.querySelector('[data-mobileMenu-open]'),
+    closeMobileBtn: document.querySelector('[data-mobileMenu-close]'),
+    mobileMenu: document.querySelector('[data-mobileMenu]')
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
+  refs.openMobileBtn.addEventListener('click', toggleMobileMenu);
+  refs.closeMobileBtn.addEventListener('click', toggleMobileMenu);
+
   function toggleModal() {
     // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
     refs.modal.classList.toggle('is-open');
+  };
+
+  function toggleMobileMenu() {
+    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
+    refs.mobileMenu.classList.toggle('is-open');
   }
 })();
